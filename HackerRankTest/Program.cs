a7962f3e420e9ad4be89524607c4a23ce9fc179a
Pablo Sebastian Andrada <pablo_sebas_006@hotmail.com>
2021-01-07 09:33:51 -0300

    BirthDayCake

--- HackerRankTest/Program.cs
+++ HackerRankTest/Program.cs
@@ -12,6 +12,7 @@ namespace HackerRankTest
             ConsoleHelper.AddOption("Plus Minus", PlusMinus.Execute);
             ConsoleHelper.AddOption("Staircase", Staircase.Execute);
             ConsoleHelper.AddOption("Mini-MaxSum", MiniMaxSum.Execute);
+            ConsoleHelper.AddOption("Birthday Cake Candles", BirthdayCakeCandles.Execute);
             ConsoleHelper.EnterTheLoop();
         }
     }
---
+++ HackerRankTest/Tests/BirthdayCakeCandles.cs
@@ -0,0 +1,68 @@
+﻿using HackerRankTest.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankTest.Tests
+{
+    public static class BirthdayCakeCandles
+    {
+
+        private static int MinSizeCandle = 1;
+        private static int MaxSizeCandle = (int)Math.Pow(10, 5);
+
+        private static int MinHeightCandle = 1;
+        private static int MaxHeightCandle = (int) Math.Pow(10,7);
+
+        private static bool IsValidCandle(int n) 
+        {
+            return (n >= MinSizeCandle && n <= MaxSizeCandle);
+        }
+
+        private static bool IsValidHeightCandle(int height) 
+        {
+            return height >= MinHeightCandle && height <= MaxHeightCandle;
+        }
+
+        private static int birthdayCakeCandles(List<int> candles)
+        {
+            var candlesAux = candles.ToArray();
+            Array.Sort(candlesAux);
+            Array.Reverse(candlesAux);
+            
+
+            int tallest = 0;
+
+            for (int i = 0; i < candlesAux.Length; i++)
+            {
+                if (IsValidHeightCandle(candlesAux[i]))
+                {
+                    tallest = candlesAux[i];
+                    break;
+                }
+            }
+
+            return candles.Count(wh => wh == tallest);
+        }
+
+        public static void Execute()
+        {
+            var lst = new List<int>
+            {
+                18,
+                90,
+                90,
+                13,
+                90,
+                75,
+                8,
+                90,
+                43
+
+            };
+            ConsoleHelper.WL($"{birthdayCakeCandles(lst)}");
+        }
+
+        
+    }
+}
