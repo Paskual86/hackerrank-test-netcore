a3e860414ab2f2b2d21f0fb9f01ad00d047793bb
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-01-18 08:51:40 -0300

    Add new test

--- HackerRankTest/Tests/ArrayManipulation.cs
+++ HackerRankTest/Tests/ArrayManipulation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HackerRankTest.Tests
 {
@@ -33,109 +34,43 @@ namespace HackerRankTest.Tests
 
         }
 
-
+        /// <summary>
+        /// Complexity O (N+K)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="queries"></param>
+        /// <returns></returns>
         static long arrayManipulation(int n, int[][] queries)
         {
-            long[] arr_result = new long[n];
+            long[] arr_result = new long[n+2];
 
             int indexFrom;
             int indexTo;
             int value;
 
             int M = queries.Length;
-            int row = 0;
-            while (M-- > 0) 
+
+
+            for (int row = 0; row < M; row++)
             {
-                indexFrom = queries[row][0] - 1;
-                indexTo = queries[row][1] - 1;
+                indexFrom = queries[row][0];
+                indexTo = queries[row][1];
                 value = queries[row][2];
-                arr_result = FillArray(arr_result, indexFrom, indexTo, value);
-                row++;
+
+                arr_result[indexFrom] += value;
+                arr_result[indexTo + 1] -= value;
             }
 
-            return GetMaximun(arr_result);
-        }
-
-        private static long[] FillArray(long[] arr, int indexFrom, int indexTo, int value) 
-        {
-            long[] result = new long[arr.Length];
-
-            Array.Copy(arr, result, arr.Length);
-
-            int index = indexFrom;
-
-            while (index <= indexTo) 
+            long accumulated =0;
+            long max = 0;
+            for (int i = 0; i < arr_result.Length; i++)
             {
-                result[index] += value;
-                index++;
+                accumulated += arr_result[i];
+                max = Math.Max(max, accumulated);
             }
-            return result;
+            return max;
         }
 
-
-        private static long[][] FillArray(long[][] arr, int FirstRow, int indexFrom, int indexTo, long value) 
-        {
-            long[][] result = new long[arr.Length][];
-            for (int row = 0; row < arr.Length; row++)
-            {
-                result[row] = new long[arr[row].Length];
-                if ((row != 0) && (row + 1 >= FirstRow))
-                {
-                    for (int col = 0; col < arr[row].Length; col++)
-                    {
-                        if (col >= indexFrom && col <= indexTo)
-                        {
-                            result[row][col] += arr[row][col] + value;
-                        }
-                        else
-                        {
-                            result[row][col] = arr[row][col];
-                        }
-                    }
-                }
-                else
-                {
-                    for (int col = 0; col < arr[row].Length; col++)
-                    {
-                        result[row][col] = arr[row][col];
-                    }
-                }
-            }
-
-            PrintMatrix(result);
-
-            return result;
-        }
-
-        private static void PrintMatrix(long[][] matrix) 
-        {
-            for (int row = 0; row < matrix.Length; row++)
-            {
-                for (int col = 0; col < matrix[row].Length; col++)
-                {
-                    Console.Write($"{matrix[row][col]} ");
-                }
-                Console.WriteLine();
-            }
-
-        }
-
-        private static bool IsValidK(long k)
-        {
-            return k >= MinValueOfK && k <= MaxValueOfK;
-        }
-
-        private static long GetMaximun(long[] arr) 
-        {
-            long result = 0;
-
-            for (int row = 0; row < arr.Length; row++)
-            {
-                if (result < arr[row]) result = arr[row];
-            }
-
-            return result;
-        }
 
     }
 }
